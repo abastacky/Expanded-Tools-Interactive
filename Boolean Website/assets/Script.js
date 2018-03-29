99efcbc57a1f3dbf78267e3227d03a3c0b19ba80
Annika <abastacky@cca.edu>
2018-03-29 09:20:39 -0700

    Many changes
    Many changes

--- Boolean Website/assets/Script.js
+++ Boolean Website/assets/Script.js
@@ -1,28 +1,88 @@
 $(document).ready(function(){
 
+	// var scrollPos;
+
+
+	// $(window).scroll(function(){
+
+	// 	scrollPos = $('body').scrollTop();
+	// 	$("#markscontainer").append("<div></div>");
+	// 	// $("body").css("height", scrollPos + "vh");
+	// 	var n = $('#markscontainer>div').length;
+	// 	$('.counter').text(n);
+
+	// 	console.log(n);
+
+	// });
+
 	var d=true;
-	$('#yesbutton').click(function(){
+	$("#yesbutton").click(function(){
 		if (d==true) {
             $(this).animate({
-			height: '300px',
-			width: '100px'
+			height: '200px',
+			width: '150px',
+		});
+            $('.counter').show();
+            $('.scrolltext').show();
 
+        // $('.counter').hide();    
+
+		var scrollPos;
+
+
+		$(window).scroll(function(){
+
+		scrollPos = $('body').scrollTop();
+		$("#markscontainer").append("<div></div>");
+		// $("body").css("height", scrollPos + "vh");
+		var n = $('#markscontainer>div').length;
+		// $('.counter').show();
+		$('.counter').text(n);
+
+		console.log(n);
 
 		});
+
+  //       var n = $('#markscontainer>div').length;
+		// $('.counter').text(n);
+
+		// console.log(n);
+
+        $("body").css("margin-top", "100px");
+        $("p").css("text-color", "red");
+
+        $(this).html("<p>N<br>O</p>");
+
+        $('.getto1000000').slideUp();
+        $('.scrollingisdrawing').slideUp();
+            
             d=false;
         }
         else {
             $(this).animate({
 			height: '100px',
-			width: '300px'
+			width: '250px'
 		});
+
+            $("body").css("margin-top", "auto");
+            $(this).html("<p>YES</p>");
+            $('.getto1000000').slideDown();
+            $('.scrollingisdrawing').slideDown();
+            $('.counter').hide();
             d=true;
+            //how do I bring the counter back to zero?
+            n=0;
         }
     });
 
     $("#yesbutton").click(function(){
         $("#drawing").slideToggle("slow");
     });
+
+
+	
+
+
 
     // $('#yesbutton').click(function(){
     // 	$(this).toggleClass('.nobutton');
